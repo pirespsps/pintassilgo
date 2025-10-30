d8078b813c110c4f85f57128c39e9120ef834f99
lolbitto <rafae.juck@gmail.com>
2025-10-30 18:10:03 -0300

    Colocando barra

---
+++ lib/components/appbar.dart
@@ -0,0 +1,71 @@
+import 'dart:collection';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Appbar extends StatefulWidget {
+  const Appbar({super.key});
+
+  @override
+  State<Appbar> createState() => _AppbarState();
+}
+
+class _AppbarState extends State<Appbar> {
+  bool isErect = false;
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+     color: colorScheme.secondary ,
+     child: Padding(
+        padding: EdgeInsetsGeometry.fromLTRB(0, 45, 10, 0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.add_circle_outline_rounded),
+              color: colorScheme.onSecondary,
+              iconSize: 40,
+            ),
+            SizedBox(
+              width: 2,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                color: colorScheme.onSecondary
+              ),
+              child: SizedBox(
+                width: 196,
+                height: 40,
+                child: Form(
+                  child: Row(
+                    
+                  )
+                ),
+              )
+            ),
+            SizedBox(
+              width: 2,
+            ),
+            IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.filter_list),
+              color: colorScheme.onSecondary,
+              iconSize: 40,
+            ),
+            IconButton(
+              onPressed: () => {},
+              icon: Icon(Icons.settings),
+              color: colorScheme.onSecondary,
+              iconSize: 40,
+            ), 
+          ],
+        ),
+      ), 
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:pintassilgo/components/appbar.dart';
 
 const AMARELO = Color.fromARGB(255, 255, 213, 42);//"#ffd52a" 
 
 const MARROM = Color.fromARGB(255, 43, 27, 18); // "#2b1b12"
 const MARROM_CLARO = Color.fromARGB(255,36, 23, 16); // #24170f
 
-const BRANCO = Color.fromARGB(255, 248, 248, 248); //"#f8f8f8"
+const BRANCO = Color.fromARGB(255, 250, 250, 246); //"#fafaf6"
 const CINZA = Color.fromARGB(255, 223, 223, 223);//"#dfdfdf"
 
 const VERMELHO = Color.fromARGB(255, 232, 65, 42); //#e8412a
@@ -42,12 +43,11 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.leave_bags_at_home),
-        backgroundColor: Theme.of(context).colorScheme.secondary,
-        title: Text(widget.title),
-      ),
-      body: Center()
+      body: Column(
+        children: [
+          Appbar()
+        ],
+      )
     );
   }
 }
