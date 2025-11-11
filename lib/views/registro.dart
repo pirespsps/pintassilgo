5ba5f44feee1a456e133583c76cd2255f785a0af
Lucas <lucasdiasmaschio@gmail.com>
2025-11-11 11:02:10 -0300

    tela de registro arrumada

--- lib/views/registro.dart
+++ lib/views/registro.dart
@@ -28,32 +28,39 @@ class _RegistroState extends State<Registro> {
 
     Size size = MediaQuery.of(context).size;
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        SizedBox(
-          height: size.height * 15 / 100,
-          width: size.width,
-          child: Center(
-            child: Image(image: AssetImage('images/icon.jpg'),),
-          ),
-        ),
-        SizedBox(
-          height: size.height * 85 / 100,
-          width: size.width,
-          child: Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.only(topRight: Radius.circular(50)),
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          SizedBox(
+            height: size.height * 25 / 100,
+            width: size.width,
+            child: Center(
+              child: Image(image: AssetImage('images/icon.jpg'),),
             ),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                children: [
-                  Center(child: Text('registro', style: TextStyle( fontSize: 60, color: Theme.of(context).colorScheme.primary, decoration: TextDecoration.none)),),
-                  Center(
-                    child: Field(
+          ),
+          SizedBox(
+            height: size.height * 75 / 100,
+            width: size.width,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.primary,
+                borderRadius: BorderRadius.only(topRight: Radius.circular(50)),
+              ),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  spacing: 15,
+                  children: [
+                    SizedBox(
+                      height: size.width * 10/100,
+                    ),
+                    Text('registro', style: TextStyle( fontSize: 60, color: Theme.of(context).colorScheme.onPrimary, decoration: TextDecoration.none)),
+                    SizedBox(
+                      height: size.width * 10/100,
+                    ),
+                    Field(
                       text: "usuario",
                       fieldController: _nomeController,
                       width: size.width - 50,
@@ -63,78 +70,85 @@ class _RegistroState extends State<Registro> {
                           return 'este campo é obrigatório';
                         // }else if(){
                         //   // usuario ja existe
-                        //   return 'este nome de usuário já existe';
+                        //   return 'este usuário já existe';
                         }else{
                           return null;
                         }
                       },
-                      ),
-                  ),
-                Center(
-                  child: Field(
-                    text: "senha",
-                    fieldController: _senhaController,
-                    width: size.width - 50,
-                    height: 65,
-                    validator: (value){
-                      if(value == null || value.isEmpty ){
-                        return 'este campo é obrigatório';
-                      // }else if(){
-                      //   // compara com o da outra senha
-                      //   return 'este campo e o campo de confirmação devem ser iguais';
-                      }else{
-                        return null;
-                      }
-                    },
-                  ),
-                ),
-                Center(
-                  child: Field(
-                      text: "confirme sua senha",
-                      fieldController: _confirmarSenhaController,
+                    ),
+                    Field(
+                      text: "senha",
+                      fieldController: _senhaController,
+                      
                       width: size.width - 50,
                       height: 65,
                       validator: (value){
                         if(value == null || value.isEmpty ){
                           return 'este campo é obrigatório';
                         // }else if(){
-                        //   // compara com o da senha
-                        //   return 'este campo e o campo de senha devem ser iguais';
+                        //   // compara com a senha
+                        //   return 'senha incorreta';
                         }else{
                           return null;
                         }
                       },
-                  ),
+                    ),
+
+                  Field(
+                      text: "confirme sua senha",
+                      fieldController: _confirmarSenhaController,
+                      
+                      width: size.width - 50,
+                      height: 65,
+                      validator: (value){
+                        if(value == null || value.isEmpty ){
+                          return 'este campo é obrigatório';
+                        // }else if(){
+                        //   // compara com a senha
+                        //   return 'senha incorreta';
+                        }else{
+                          return null;
+                        }
+                      },
+                    ),
+
+                    SizedBox(
+                      height: size.width * 10/100,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.secondary)
+                          ),
+                          child: Text("cadastrar",
+                            style: TextStyle(fontSize: 25),
+                          ),
+                          onPressed: () async {
+                            //entrar no site
+                          },
+                        ),
+                        TextButton(
+                          child: Text("entrar (já tenho uma conta)",
+                            style: TextStyle(
+                              fontSize: 15,
+                              color: Theme.of(context).colorScheme.onPrimary
+                            ),
+                          ),
+                          onPressed: () async {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    )
+                  ],
                 ),
-                Center(
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: WidgetStatePropertyAll(Theme.of(context).colorScheme.secondary)
-                    ),
-                    child: Text("cadastrar)",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                    onPressed: () async {
-                      //cadastrar o usuario e entrar no sistema
-                    },
-                  ),
-                ),
-                Center(
-                  child: TextButton(
-                    child: Text("entrar (já tenho conta)",
-                      style: TextStyle(fontSize: 15),
-                    ),
-                    onPressed: () async {
-                      Navigator.pop(context);
-                    },
-                  ),
-                )
-                ],
               ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
